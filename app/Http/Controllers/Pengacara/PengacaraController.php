c6218bd4502fcb03f114bb7deca3e37bfefe5af7
ariefraihandi <raihandi93@gmail.com>
2024-06-01 00:40:11 +0700

    push

---
+++ app/Http/Controllers/Pengacara/PengacaraController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Pengacara;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PengacaraController extends Controller
+{
+    //
+}
