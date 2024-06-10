ddd4eaed0bcf857a2256bb92e25254d9f47e2c34
ariefraihandi <raihandi93@gmail.com>
2024-06-10 07:22:29 +0700

    de

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -130,8 +130,6 @@ class AuthController extends Controller
             $url = "https://bilikhukum.com/verify-email?uniqueid=";
         }
 
-        
-
         $data = [
             'meta_description' => 'Daftarkan kantor hukum Anda dan bergabunglah bersama bilikhukum.com. Kami menyediakan platform untuk pengacara, notaris dan mediator profesional yang siap membantu berbagai masalah hukum, mulai dari perkara pidana, perdata, hingga bisnis. Konsultasi gratis tersedia.',
             'meta_keywords' => 'pengacara, jasa pengacara, konsultasi pengacara, bantuan hukum, pengacara pidana, pengacara perdata, pengacara bisnis',
